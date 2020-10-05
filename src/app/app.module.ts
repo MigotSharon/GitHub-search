7a98ea6950608360bfce8d40e6a73a76f1996669
SharonMigot <migotsharon@gmail.com>
2020-10-05 12:38:37 +0300

    generated component open

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{ FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormComponent } from './form/form.component';
+import { OpenComponent } from './open/open.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FormComponent
+    FormComponent,
+    OpenComponent
   ],
   imports: [
     BrowserModule,
@@ -20,3 +23,13 @@ import { FormComponent } from './form/form.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
+
+
+
+
+ 
